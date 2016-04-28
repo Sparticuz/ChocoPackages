41c8172f58bb44629606962baeccfba85e325d91
Kyle McNally <kyle@kmcnally.net>
2016-04-28 12:13:08 -0400

    Add Junction Link Magic

---
+++ junction-link-magic/tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'junction-link-magic'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.rekenwonder.com/linkmagic2.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+  softwareName  = 'junction-link-magic*'
+  checksum      = 'fce8a96cf32037fac594c3e441febcc0'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
